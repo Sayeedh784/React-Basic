575064261d3316185982ba513f23a0052123128d
Sayeedh784 <sayeedh74@gmail.com>
2021-10-21 21:12:45 +0600

    2nd

---
+++ src/mycomponents/Hello.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+function Hello(){
+  return(
+
+    <h1>Hello I am from functional component.</h1>
+
+  );
+
+
+
+}
+export default Hello;
---
+++ src/mycomponents/welcome.js
@@ -0,0 +1,12 @@
+import React,{ Component } from 'react'
+
+class Welcome extends Component{
+  render(){
+    return <h1>Welcome to class function.</h1>
+    
+  }
+    
+}
+
+export default Welcome;
+
